57cb3a451f6303838b2aca73b38e0f7c166c1dd2
Kun Zhao <kunzhao@Kuns-MacBook-Air.local>
2021-03-06 15:18:40 -0500

    Update the code

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scraping
+import scraping  # import scraping.py
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
+# Connect to Mongo and select mars_app Database and NOT A COLLECTION!!!!
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+# To see the DB
 
 
 @app.route("/")
 def index():
+    # Use PyMongo to find the 'mars' collection in the database and assgin the path to variable mars
     mars = mongo.db.mars.find_one()
+    # Render the mongo data in the HTML index
     return render_template("index.html", mars=mars)
+
+# To update the DB
 
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
+    mars = mongo.db.mars  # access the database, scrape new data using our scraping.py
+    mars_data = scraping.scrape_all()  # the function scrap_all() works on scraping.py
     mars.update({}, mars_data, upsert=True)
     return redirect('/', code=302)
 
 
+# run flask
 if __name__ == "__main__":
     app.run()
